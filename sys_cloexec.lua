d47912bf9ca6ea1526c0227fe9e1f90561e1d6ec
Chen Yufei <cyfdecyf@gmail.com>
2014-03-26 14:41:05 +0800

    Add close exec wrap for accept.

--- sys_cloexec.lua
+++ sys_cloexec.lua
@@ -21,4 +21,21 @@ function M.socket(family, sotype, proto)
   return sockfd, nil
 end
 
+-- Wrapper around the accept system call that marks the returned file
+-- descriptor as nonblocking and close-on-exec.
+function M.accept(sockfd)
+  local nfd, sockaddr, err = sys.accept(sockfd)
+  if err then
+    return nil, nil, err
+  end
+
+  sys.close_on_exec(nfd)
+  err = sys.set_nonblock(nfd, true)
+  if err then
+    sys.close(nfd)
+    return nil, nil, err
+  end
+  return nfd, sockaddr, nil
+end
+
 return M
